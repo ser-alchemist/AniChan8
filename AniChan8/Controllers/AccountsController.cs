17a3c2fe38ff323527a313cfed79590730878028
Jerin Ahasan Kheya <111109553+misakiMeo@users.noreply.github.com>
2022-08-19 12:28:30 +0600

    Add files via upload

---
+++ AniChan8/Controllers/AccountsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AniChan8.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+
+    }
+}
