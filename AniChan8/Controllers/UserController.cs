bb35e65dc75ea4d3044074119dd42710345a3da3
ser-alchemist <94074738+ser-alchemist@users.noreply.github.com>
2022-08-21 01:22:11 +0600

    Add files via upload

---
+++ AniChan8/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AniChan8.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Bookmark()
+        {
+            return View();
+        }
+    }
+}
