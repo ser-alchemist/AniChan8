4e261f2b939b8dbba332cdae7dbc6e09e270049b
Jerin Ahasan Kheya <111109553+misakiMeo@users.noreply.github.com>
2022-09-03 16:08:20 +0600

    Show Bookmark Done; controller level

--- AniChan8/Controllers/UserController.cs
+++ AniChan8/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using AniChan8.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +11,17 @@ namespace AniChan8.Controllers
     public class UserController : Controller
     {
         // GET: User
+        AniChanEntities1 db = new AniChanEntities1();
         public ActionResult Bookmark()
         {
-            return View();
+            var bookMarkList = db.Bookmarks.SqlQuery("select * from Bookmark where user_id_=@user", new SqlParameter("@user", Session["user_id_SS"])).ToList();
+            List <Anime> animeList = new List<Anime>();
+            foreach(var entry in bookMarkList)
+            {
+                var anime = db.Animes.SqlQuery("select * from Anime where anime_id=@anime", new SqlParameter("@anime", entry.anime_id)).FirstOrDefault();
+                animeList.Add(anime);
+            }
+            return View(animeList);
         }
     }
 }
